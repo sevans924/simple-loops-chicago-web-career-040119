b9221cc501cfa4ea1ccaedc8b1fb2b9dee0df304
Sarah Elzinga <sevans924@gmail.com>
2019-03-21 19:38:53 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -2,11 +2,10 @@
 
 def loop_iterator(number_of_times)
 
-  phrase = "Welcome to Flatiron School's Web Development Course!"
   counter = 0
   loop do
     counter +=1
-    puts phrase
+    puts "Welcome to Flatiron School's Web Development Course!"
     if counter >= number_of_times
       breaks
     end
